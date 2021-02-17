fc4d866dcfe39734f5292f8d69a05340a84e3df7
koichi.hasegawa <koichi.hasegawa@camp-fire.jp>
2021-02-17 15:48:55 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+export const title = 'WebフロントTIPS'
+export const pageWidth = 1600
+export const pageHeight = 900
+export const pageTotal = 20
+export const getPageUrl = (page) => `slides/${page}.html`
